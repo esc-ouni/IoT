3918ac15ea85a8019fde8b369a54e9aeae088b05
esc-ouni <idouni@student.1337.ma>
2026-02-23 02:49:53 +0000

    v for unix like systems -- updating

---
+++ src/p1/.scipts/ServerWorker_setup.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apk update
+sudo apk vim htop curl
+curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true sh -s - server --disable-agent
+sed -i 's/respawn_max=0/respawn_max=10/g' /etc/init.d/k3s
+rc-service k3s start
