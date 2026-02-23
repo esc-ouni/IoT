0f2dea4d8d23d87ca06121fbaa236128dbfd0a72
esc-ouni <idouni@student.1337.ma>
2026-02-23 23:11:05 +0000

    tbc -- updating

--- src/p1/.scipts/ServerWorker_setup.bash
+++ src/p1/.scipts/ServerWorker_setup.bash
@@ -3,4 +3,6 @@ sudo apk update
 sudo apk add vim htop curl
 curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true sh -s - server --disable-agent
 sed -i 's/respawn_max=0/respawn_max=10/g' /etc/init.d/k3s
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 rc-service k3s start
+alias k='k3s kubectl'
